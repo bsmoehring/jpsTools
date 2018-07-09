f41f16239b7cb366be6ccb241e7f2c99626e4cc2
bsmoehring <bsmoehring@gmail.com>
2018-07-09 09:35:39 +0200

    some work about plots

--- jpsTools/analysis/RunStatistics.py
+++ jpsTools/analysis/RunStatistics.py
@@ -6,7 +6,7 @@ import math
 #from Agents import Agents, Source
 #from constants import jps
 
-def plotTimeVariationForGroup(outputFolder, column, platformFrom, platformTo,  input_list = []):
+def plotTimeVariationForGroup(outputFolder, column, platformFrom = None, platformTo = None,  input_list = []):
 
     # assert isinstance(agents, Agents)
     #
@@ -27,13 +27,19 @@ def plotTimeVariationForGroup(outputFolder, column, platformFrom, platformTo,  i
         color = inputDic['color']
         opacity = inputDic['opacity']
 
-        changes = pd.read_csv(file, sep=';', converters={'secondsInSim':float, 'time':int})
+        changes = pd.read_csv(file, sep=';', converters={column:float, 'time':int})
+        changes = changes.dropna(subset=[column])
         selectedChanges = changes.loc[
-            ((changes['platformFrom'] == platformFrom) & (changes['platformTo'] == platformTo))
-            | ((changes['platformTo'] == platformFrom) & (changes['platformFrom'] == platformTo))
-            & (changes[column] > 20)
-            & (changes['time'] >= 240)
-            & ((changes['area_11'] == True) | (changes['area_21'] == True) | (changes['area_31'] == True))
+            (changes['time'] >= 300)
+#            & ((changes['area_11'] == True) | (changes['area_21'] == True) | (changes['area_31'] == True))
+        ]
+        if platformFrom is not None:
+            selectedChanges = selectedChanges.loc[
+                (changes['platformFrom'] == platformFrom) | (changes['platformTo'] == platformFrom)
+            ]
+        if platformTo is not None:
+            selectedChanges = selectedChanges.loc[
+                (changes['platformTo'] == platformTo) | (changes['platformFrom'] == platformTo)
             ]
         plotAverage(plt, column, selectedChanges, color, 0)
 
@@ -52,7 +58,7 @@ def plotTimeVariationForGroup(outputFolder, column, platformFrom, platformTo,  i
     #plt.yticks(np.arange(0, 400, 50))
     plt.xticks(np.arange(0, 360, 50))
     plt.title(column)
-    #plt.show()
+    plt.show()
     plt.draw()
     plt.savefig(outputFolder + platformFrom + '_' + platformTo + '_' + column + '.png')
     plt.close()
@@ -148,80 +154,6 @@ def plotMax(plt, df, area, color, offset, fps):
     )
     return maxY_X
 
-def plotAreaDensity(input_list, area, areas, fps, path):
-    plt.rcParams.update({'font.size': 14})
-    outputfile = path + 'densityinarea'+area
-    column = area+'_avgA'
-    #plt.title('Detail '+area[0])
-    plt.xlabel('Zeit in Sekunden')
-    plt.xticks(np.arange(300, 1801, 300))
-    plt.yticks(np.arange(0., 3.5, 0.5))
-    axes = plt.gca()
-    axes.set_ylim(0.5, 3.25)
-    plt.ylabel('$m^2$ / Person')
-
-    for input in input_list:
-        color = input['color']
-        label = input['label']
-        offset = input['offset']
-        file = input['file']+'areaLoS.csv'
-        converters = {'frame': int}
-        for area1 in areas:
-            converters[str(area1)+'_numb'] = int
-            converters[str(area1)+'_minA'] = float
-            converters[str(area1)+'_maxA'] = float
-            converters[str(area1)+'_avgA'] = float
-        frames = pd.read_csv(file, sep=';', converters=converters)
-        selectedFrames = frames.loc[
-            ((frames['frame'] % (fps) == 0)) & (frames['frame']>300*fps) & (frames['31_numb'] > 10)
-        ]
-        selectedFramesMeans = selectedFrames.groupby(
-            pd.cut(selectedFrames['frame'], np.arange(0, 14000 + (20 * fps), 20 * fps))
-        ).mean()
-        plt.plot(
-            selectedFramesMeans['frame'] / fps, selectedFramesMeans[column],
-            color=color, label=label, alpha=0.9, linewidth=0.7
-        )
-        ylim = plt.axes().get_ylim()[1]
-        mean = selectedFrames[column].mean()
-        #plt.axhline(y=mean, color=color, linewidth=1.5)
-        plt.text(
-            x=280., y= 1.0*(mean/ylim*ylim)-offset/15,
-            s='\u2205: '+str(round(mean,2)),
-            horizontalalignment='center',
-            verticalalignment='center',
-            color=color,
-            bbox=dict(facecolor='white', alpha=1.0)
-        )
-        minY = selectedFrames[column].min()
-        minY_X = selectedFrames.loc[selectedFrames[column].idxmin()]['frame']
-        plt.axvline(x=minY_X/fps, color=color, linewidth=1.5)
-        # plt.text(
-        #     x=minY_X/fps, y=1*plt.axes().get_ylim()[0],
-        #     s='Sek: \n' + str(int(round(minY_X / fps, 0))),
-        #     horizontalalignment='center',
-        #     verticalalignment='center',
-        #     color=color,
-        #     bbox=dict(facecolor='white', alpha=1.0)
-        # )
-        y = (minY / ylim * ylim)-(offset/15) -0.1
-        if y < 0.7: y = 0.7
-        plt.text(x=minY_X / fps, y=y,
-                 s=str(minY) + ' $m^2$'+'\nSek: ' + str(int(minY_X/fps)),
-                 horizontalalignment='center',
-                 verticalalignment='center',
-                 color=color,
-                 bbox=dict(facecolor='white', alpha=1.0)
-                 )
-        print(area, label, minY, int(minY_X))
-    leg  = plt.legend()
-    for legobj in leg.legendHandles:
-        legobj.set_linewidth(2.0)
-
-    plt.savefig(outputfile)
-    plt.show()
-    plt.close()
-
 def runAggregateStatistics(input_csv, label, arealist, area_quality_dic):
     changes = pd.read_csv(
         input_csv, sep=';',
@@ -250,7 +182,7 @@ def runAggregateStatistics(input_csv, label, arealist, area_quality_dic):
             & (changes[area_column]==True)
         ]
         area_mean = round(selected_changes[minArea_column].mean(), 2)
-
+        area_quality_dic[area]['N'].append(selected_changes.shape[0])
         area_quality_dic[area]['A'].append(selected_changes.loc[(selected_changes[minArea_column] >= 3.24)].shape[0])
         area_quality_dic[area]['B'].append(selected_changes.loc[
             (selected_changes[minArea_column] < 3.24) & (selected_changes[minArea_column] >= 2.32)].shape[0])
@@ -273,51 +205,169 @@ def runAggregateStatistics(input_csv, label, arealist, area_quality_dic):
             'E', area_quality_dic[area]['E'][-1],
             'F', area_quality_dic[area]['F'][-1]
         )
+def plotAreaDensity(input_list, areas, fps, path):
+    f, ax = plt.subplots(1, 3, sharey=True, sharex=True, figsize=(10, 3))
+    f.subplots_adjust(bottom=0.2)  # make room for the legend
+    plt.gca().set_ylim(0.4, 3.25)
+    plt.yticks(np.arange(0.5, 3.5, 0.5))
+    plt.xticks(np.arange(300, 1801, 500))
+    lines = []
+    labels=[]
+    #plt.rcParams.update({'font.size': 14})
+    outputfile = path + 'densityinarea.png'
 
-def plot_minAreas(areas, input_list, qualities):
+    def create_subplot(column, axis, area):
+        axis.set_title('Detail '+str(area)[0])
+        axis.yaxis.grid(color='lightgrey', linewidth=0.5)
+        axis.tick_params(pad=10)
+        for input in input_list:
+            color = input['color']
+            label = input['label']
+            offset = input['offset']
+            file = input['file'] + 'areaLoS.csv'
+            converters = {'frame': int}
+            converters[str(area) + '_numb'] = int
+            converters[str(area) + '_minA'] = float
+            converters[str(area) + '_maxA'] = float
+            converters[str(area) + '_avgA'] = float
+            frames = pd.read_csv(file, sep=';', converters=converters)
+            selectedFrames = frames.loc[
+                ((frames['frame'] % (fps) == 0)) & (frames['frame'] > 300 * fps)
+                ]
+            selectedFramesMeans = selectedFrames.groupby(
+                pd.cut(selectedFrames['frame'], np.arange(0, 14000 + (20 * fps), 20 * fps))
+            ).mean()
+            lines.append(axis.plot(
+                selectedFramesMeans['frame'] / fps, selectedFramesMeans[column],
+                color=color, label=label, alpha=0.9, linewidth=0.7
+            ))
+            ylim = plt.gca().get_ylim()[1]
+            mean = selectedFrames[column].mean()
+            y = 1.0 * (mean / ylim * ylim) - offset / 7
+            # plt.axhline(y=mean, color=color, linewidth=1.5)
+            if label == 'Basis':
+                axis.text(
+                    x=270, y=y+0.35,
+                    s='avg:',
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    color='black',
+                    bbox=dict(facecolor='white', alpha=1.0)
+                )
+            axis.text(
+                x=270, y=y,
+                s= str(round(mean, 2)),
+                horizontalalignment='center',
+                verticalalignment='center',
+                color=color,
+                bbox=dict(facecolor='white', alpha=1.0)
+            )
+            minY = selectedFrames[column].min()
+            minY_X = selectedFrames.loc[selectedFrames[column].idxmin()]['frame']
+            axis.axvline(x=minY_X / fps, color=color, linewidth=1.5, ymax=(minY / ylim ))
+            if area == 31 and label == 'Basis': y=0.5
+            else: y = 0.1
+            axis.text(
+                x=minY_X/fps, y=y,
+                s=str(int(round(minY_X / fps, 0))),
+                horizontalalignment='center',
+                verticalalignment='center',
+                color=color,
+                bbox=dict(facecolor='white', alpha=1.0)
+            )
+            y = (minY / ylim * ylim)
+            if area == 31 and label == 'Basis': y-=offset/20
+            if y < 0.7: y = 0.7
+            axis.text(x=minY_X / fps, y=y,
+                     s=str(minY),
+                     horizontalalignment='center',
+                     verticalalignment='center',
+                     color=color,
+                     bbox=dict(facecolor='white', alpha=1.0)
+                     )
+            # print(area, label, minY, int(minY_X))
+
+    for area in areas:
+        create_subplot(column = str(area)+'_avgA', axis=ax[areas.index(area)], area=area)
+
+    ax[0].set_ylabel('$m^2$ / Person')  # add left y label
+    ax[0].set_ybound(0, 3.25)  # add buffer at the top of the bars
+    ax[0].set_xlabel('Zeit in Sekunden')
+    ax[2].set_xlabel('Zeit in Sekunden')
+    handles, labels = ax[0].get_legend_handles_labels()
+    leg = f.legend(handles, labels, loc='lower center', ncol=3)
+    for legobj in leg.legendHandles:
+        legobj.set_linewidth(2.0)
+    f.tight_layout()
+    plt.savefig(outputfile)
+    plt.show()
+    plt.close()
+
+def plot_minAreas(areas, input_list, qualities, path):
     area_quality_dic = {}
     for area in areas:
-        area_quality_dic[area] = {}
+        area_quality_dic[area] = {'N':[]}
         for quality in qualities:
             area_quality_dic[area][quality] = []
     for input in input_list:
         runAggregateStatistics(input['file'] + 'changeTimes.csv', input['label'], areas, area_quality_dic)
 
     colors = ['#fff5f0', '#fcccb7', '#fb8e6e', '#f34d37', '#c4161b', '#67000d']
-    labels = qualities
+    model_names = ['Basis', 'Prog 1', 'Prog 2']
     ind = np.arange(3)
-    width = 0.5
-    f, ax = plt.subplots(1, 3, sharey=True, sharex=True)
-    f.subplots_adjust(bottom=0.2)  # make room for the legend
+    f, ax = plt.subplots(1, 3, sharey=True, sharex=True, figsize=(5, 4))
     plt.yticks(np.arange(0, 4500, 500))
-    plt.xticks(ind + width / 2., ('Basis', '1', '2'))
+    #plt.xticks(ind, model_names)
     p = []  # list of bar properties
+    m = [] # list if model properties
 
-    def create_subplot(matrix, colors, axis, title):
+    def create_subplot(matrix, colors, axis, title, n, model_names):
         bar_renderers = []
+        model_renderers = []
         ind = np.arange(matrix.shape[1])
         bottoms = np.cumsum(np.vstack((np.zeros(matrix.shape[1]), matrix)), axis=0)[:-1]
+        for i in ind:
+            print(i, input_list[i]['color'])
+            r = axis.bar(model_names[i], n[i], width=0.8, fill=False
+                     , linewidth=3, edgecolor=input_list[i]['color']
+                     )
+            model_renderers.append(r)
         for i, row in enumerate(matrix):
-            r = axis.bar(ind, row, width=0.5, color=colors[i], bottom=bottoms[i])
+            r = axis.bar(ind, row, width=0.7, color=colors[i], bottom=bottoms[i])
             bar_renderers.append(r)
         axis.set_title(title)
-        return bar_renderers
+        axis.get_xaxis().set_visible(False)
+        #plt.sca(axis)
+        #plt.xticks(rotation=45)
+        return bar_renderers, model_renderers
 
     for area in areas:
         values = []
+        n = []
         for quality in qualities:
             values.append(area_quality_dic[area][quality])
+        n.extend(area_quality_dic[area]['N'])
+        bar_renderers, model_renderers = create_subplot(np.array(values), colors, ax[areas.index(area)], 'Detail '+str(area)[0], n, model_names)
+        p.extend(bar_renderers)
+        m.extend(model_renderers)
 
-        p.extend(create_subplot(np.array(values), colors, ax[areas.index(area)], 'Detail '+str(area)))
-
-    ax[0].set_ylabel('Instructions Executed')  # add left y label
-    ax[0].set_ybound(0, 4500)  # add buffer at the top of the bars
+    ax[0].set_ylabel('Anzahl Agenten')  # add left y label
+    ax[0].set_ybound(0, 4400)  # add buffer at the top of the bars
+    f.legend(((x[0] for x in m)),  # bar properties
+             ('Basis', 'Prognose 1', 'Prognose 2'),
+             bbox_to_anchor=(0.55, 0.065),
+             loc='lower center',
+             ncol=3)
     f.legend(((x[0] for x in p)),  # bar properties
-             (labels),
-             bbox_to_anchor=(0.5, 0),
+             (qualities),
+             bbox_to_anchor=(0.55, 0),
              loc='lower center',
              ncol=6)
+    f.tight_layout()
+    f.subplots_adjust(bottom=0.15)  # make room for the legend
+    plt.savefig(path+'minAreas.png')
     plt.show()
+    plt.close()
 
 
 def nameEarlyAgents(input_list, platformFrom, platformTo):
@@ -347,14 +397,19 @@ if __name__ == "__main__":
 
     input= '/media/bsmoehring/Data/wichtiges/tuberlin/masterarbeit/runs/'
     inputBasis = {'file':input + '0_ipfDemandBasic/', 'label':'Basis', 'color':'grey', 'opacity':0.4, 'offset':-2}
-    inputProg1 = {'file':input + '1_ipfDemandProg1/', 'label':'Prognose 1', 'color':'blue', 'opacity':0.25, 'offset':0}
-    inputProg2 = {'file':input + '2_ipfDemandProg2/', 'label':'Prognose 2', 'color':'orange', 'opacity':0.3, 'offset':+2}
+    inputProg1 = {'file':input + '1_ipfDemandProg1/', 'label':'Prog 1', 'color':'orange', 'opacity':0.25, 'offset':0}
+    inputProg2 = {'file':input + '2_ipfDemandProg2/', 'label':'Prog 2', 'color':'darkred', 'opacity':0.3, 'offset':+2}
     input_list = [inputBasis, inputProg1, inputProg2]
 
     #plotTimeVariationForGroup(
     #        outputFolder=input+'analysis/', column='secondsInSim',
     #        platformFrom='S', platformTo='U2',
     #        input_list=input_list)
+    plotTimeVariationForGroup(
+            outputFolder=input+'analysis/', column='secondsInSim',
+            platformFrom=None, platformTo=None,
+            input_list=input_list
+    )
 
     # for platformFrom, platformTo in itertools.combinations(platforms, 2):
     #     if platformFrom == 'S' and platformTo == 'Regio':
@@ -364,8 +419,7 @@ if __name__ == "__main__":
     #         platformFrom=platformFrom, platformTo=platformTo,
     #         input_list=input_list
     #     )
-    #
-    #
+
     # for area in areas:
     #     plotFrameAreaAgents(
     #         input+'analysis/agentsinarea'+str(area)+'.png',
@@ -382,10 +436,9 @@ if __name__ == "__main__":
 
     #nameEarlyAgents(input_list, 'Dircksenstr.', 'U8')
 
-    #for area in areas:
-    #    plotAreaDensity(input_list, str(area), areas, 8, input+'analysis/')
+    #plotAreaDensity(input_list, areas, 8, input+'analysis/')
 
-    plot_minAreas(areas, input_list, qualities)
+    #plot_minAreas(areas, input_list, qualities, input+'analysis/')
 
 
 
